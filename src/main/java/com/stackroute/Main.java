dcb95a18c413a97a222201b2cf3da53231b00793
yasawant.k.rosan <yasawant.k.rosan@boeing.com>
2019-01-16 08:40:15 +0530

    Spring Task3

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -14,23 +14,14 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 import java.beans.beancontext.BeanContext;
-//fhgffkmdikgfdmhfd
+
 public class Main {
     public static void main(String[] args) {
-
-        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-
-        Movie movie = (Movie) factory.getBean("movie");
-        System.out.println(movie.getActor().toString());
-
-        BeanDefinitionRegistry registry = new DefaultListableBeanFactory();
-        BeanDefinitionReader reader = new XmlBeanDefinitionReader(registry);
-        reader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie1 = (Movie) ((DefaultListableBeanFactory) registry).getBean("movie");
-        System.out.println(movie1.getActor().toString());
-
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2 = context.getBean(Movie.class);
-        System.out.println(movie2.getActor());
+        Movie movie = (Movie) context.getBean("movie");
+        System.out.println(movie.getActor());
+
+        Movie movie1 = (Movie) context.getBean("movie1");
+        System.out.println(movie1.getActor());
     }
 }
